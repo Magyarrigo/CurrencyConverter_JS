d6cbec26dfac6c0fdb92ca8f1ba91d0437a35258
Boguslaw Drozd <b.drozd07@gmail.com>
2023-06-29 10:00:28 +0200

    the logical structure is ready

--- index.js
+++ index.js
@@ -1,59 +1,67 @@
-console.log("juz jestem");
-const btn = document.querySelector(".askForCurrences");
-console.log(btn);
-//const link ='http://api.nbp.pl/api/exchangerates/rates/a/chf/';
-const link = "http://api.nbp.pl/api/exchangerates/tables/a/";
-btn.addEventListener("click", getCurrencyListByAxios);
-let kursDolara = 0;
-let kursFranka = 0;
+"use strict";
 
-function getCurrencyListByAxios() {
+const link = "http://api.nbp.pl/api/exchangerates/tables/a/";
+const dataEntryForm = document.forms[0];
+let exchangeResult = document.querySelector("#conversionResult");
+
+dataEntryForm.addEventListener("submit", getCurrencyListByAxios);
+
+let targetCurrencyRate = 0;
+
+function getCurrencyListByAxios(event) {
+  event.preventDefault();
+  if (isNaN(dataEntryForm[0].value)){
+    
+    alert("wprowadź poprawną wartość: LICZBA DODATNIA");
+    clearForm();
+    return;
+
+  };
+  if (dataEntryForm[0].value <= 0) {
+    
+    alert("wprowadź poprawną wartość: LICZBA DODATNIA");
+    clearForm();
+    return;
+  }
+  //if (dataEntryForm[0].value instanceof String || dataEntryForm[0].value == null) {
+  //  console.log("hahahaha");
+  //}
+
+  const currencyCode = document.querySelector("#selectCurrency").value;
+
   axios
     .get(link)
     .then((response) => {
       const currencyList = response.data[0].rates;
-      //console.log(currencyList);
 
-      const usd = currencyList.filter((element) => {
-        return element.code === "USD";
-      });
-      const chf = currencyList.filter((element) => {
-        return element.code === "CHF";
+      const targetCurrency = currencyList.filter((element) => {
+        return element.code === currencyCode;
       });
 
-      //console.log(usd);
-      console.log("kurs dolara to: " + usd[0].mid);
-      console.log("kurs franka to:" + chf[0].mid);
-      //mam kurs dolara
-      kursDolara = usd[0].mid;
+      //console.log(dataEntryForm[0].value);
+
+      targetCurrencyRate = targetCurrency[0].mid;
+      //console.log(targetCurrencyRate);
+
+      let nowyText = currencyConversion(
+        targetCurrencyRate,
+        dataEntryForm[0].value
+      );
+      nowyText = parseFloat(nowyText).toFixed(2);
+
+      exchangeResult.innerText = `to: ${nowyText} zł`;
+
+      //console.log(exchangeResult.innerText);
     })
 
     .catch((error) => console.error(error));
-  return kursDolara;
 }
-console.log(kursDolara);
-//console.log('kurs dolara to: ' + usd[0].mid);
-//       console.log('kurs franka to:' + chf[0].mid);
 
-/*const link = 'https://api.frankfurter.app/latest';
-
-btn.addEventListener('click',getCurrencyList);
-
-function getCurrencyList(){
-    fetch(link)
-.then(res => res.json())
-.then(data => {
-    const arrayOfCurrencies = Object.keys(data.rates);
-    const select = document.createElement('select')
-    arrayOfCurrencies.forEach((currencyCode) => {
-        let option = document.createElement('option');
-        option.value = currencyCode;
-        option.innerHTML = currencyCode;
-      
-        select.appendChild(option);
-    })    
-    document.body.appendChild(select);
-})
-.catch(error => console.error(error))
+function currencyConversion(currencyRate, quantity) {
+  const conversionResult = currencyRate * quantity;
+  return conversionResult;
 }
-*/
+function clearForm() {
+  dataEntryForm[0].value = "";
+  exchangeResult.innerText = "";
+}
